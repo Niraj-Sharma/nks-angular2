0ef53b0f506d81eb5030a5fe3da4da810a86ca21
niraj-sharma <niraj_kush17@yahoo.com>
2016-05-11 15:09:26 +0530

    first level component typed class

--- app/page-footer/page-footer.ts
+++ app/page-footer/page-footer.ts
@@ -1,7 +1,18 @@
 ﻿import {Component} from '@angular/core';
 
+export class Footer { 
+	name : string; 
+	year : string ;
+}
+
 @Component({
     selector: 'page-footer',
     templateUrl: 'app/page-footer/page-footer-template.html'
 })
-export class PageFooter { }
+
+export class PageFooter {
+ footer: Footer = {
+    name: 'mycompany',
+	year :'2016'
+  };
+}
