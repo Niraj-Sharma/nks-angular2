7828c9cd5b535e5de9067b4655f4f54ac6bd2c94
niraj-sharma <niraj_kush17@yahoo.com>
2016-05-11 12:33:38 +0530

    dummy second component

---
+++ app/app.component2.ts
@@ -0,0 +1,7 @@
+﻿import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-head',
+    template: '<h1>Header Info</h1>'
+})
+export class AppComponent2 { }
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,7 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
+import {AppComponent2} from './app.component2';
+
 
 bootstrap(AppComponent);
+bootstrap(AppComponent2);
